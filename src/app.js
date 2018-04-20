ebb0e4431d2884e130c3a585d357a6023154bc52
Will Farley <wfarley@amazon.com>
2018-04-20 13:26:59 -0700

    Add solution

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ import Message from './Message';
 import Panel from './Panel';
 import './app.css';
 // TODO 1.1: import socket.io-client
+import io from 'socket.io-client';
 // TODO 1.2: create a new socket connection by invoking "socket.io-client". Convention is to name the returned socket instance "socket".
+const socket = io();
 
 /**
  * Creates a layout panel in a specified position. Other components can use
@@ -27,25 +29,45 @@ const controlPanel = createLayoutPanel('bottomLeft'); // actions on the paint ca
  * greet.write('my new message');
  *
  * @type {Message}
- * @property {Function} write update the message text
+ * @property {Function} write - update the message text
  */
 const greet = new Message({
   text: 'Thank you for joining us!',
   prefix: '👋',
   mountPoint: statusPanel,
 });
+const targetAudience = new Message({
+  text: 'All',
+  prefix: '📢',
+  mountPoint: statusPanel,
+});
 
+// TODO 2.1 (partial)
 let username; // client username
+// TODO 3.1 (partial)
+let toUser; // pm paint
 
 // initialize paint canvas
 const paintCanvas = new PaintCanvas({
   mountPoint: document.body,
   onMove({points, color}) {
     // TODO 1.3: emit a "DRAW_POINTS" message to the server when paintCanvas has mouseMove events
+    // socket.emit('DRAW_POINTS', {points, color});
+    // TODO 3.1 (partial)
+    socket.emit('DRAW_POINTS', {points, color, fromUser: username, toUser});
   },
 });
 
-// TODO 1.4: listen for draw events from the server of the draw action-type (eg "DRAW_POINTS") and use the paintCanvas.drawLine(Array<{x: number, y: number}>, color: string) method to draw the points on the canvas.
+// TODO 1.4: listen for draw events from the server of the draw action-type (eg "DRAW_LINE") and use the paintCanvas.drawLine(Array<{x: number, y: number}>, color: string) method to draw the points on the canvas.
+// socket.on('DRAW_POINTS', ({points, color}) => {
+//   paintCanvas.drawLine(points, color);
+// });
+// TODO 3.2 When a user is selected, filter draw events from other users (from 1.4) and only display events from the selected user.
+socket.on('DRAW_POINTS', ({points, color, fromUser}) => {
+  if (toUser === fromUser || !toUser) {
+    paintCanvas.drawLine(points, color);
+  }
+});
 
 // create and render the color selector
 new ColorSelector({
@@ -64,15 +86,50 @@ new Button({
   },
 });
 
-setTimeout(() => {
-  // next tick
+
+// TODO 2.1: Emit "LOGIN" event to server
+// TODO 2.2: Prevent users from using an existing username (multiple ways to do this, the most elegant would be using an "acknowledgement" when you dispatch the login event)
+const login = () => {
   username = prompt('Enter your username');
+  socket.emit('LOGIN', {username}, login);
+};
+socket.on('connect', () => {
+  login();
   greet.write(`Hello, ${username}.`);
 });
-// TODO 2.1: Emit a login event (eg "LOGIN") to the server when the client is connected with the selected username.
-// TODO 2.2: Prevent users from using an existing username (multiple ways to do this, the most elegant would be using an "acknowledgement" when you dispatch the login event)
-// TODO 2.3: Listen for an update user list event (eg "UPDATE_USER_LIST") from server, containing the "users" object with all usernames then update the dom to display this.
 
-// TODO 3.1 Update the user list display from step 2.3 so that it displays buttons, when clicked, draw events will only be dispatched to that user. You will also need to modify the onMove handler from 1.3
-// TODO 3.2 When a user is selected, filter draw events from other users and only display events from the selected user. You will likely need to update the "DRAW_POINTS" listener from 1.4
-// TODO 3.3 Create a button that, when clicked, will send draw events to all users again.
+// TODO 2.3: Listen for an update user list event (eg "UPDATE_USER_LIST") from server, containing the "users" object with all usernames and update user list display. There's a "usersElem" variable that you can just set "innerHTML" value to display new users
+// const createUserMessage = username => new Message({
+//   text: username,
+//   mountPoint: userPanel,
+// });
+// socket.on('UPDATE_USER_LIST', ({users}) => {
+//   userPanel.clear();
+//   Object.keys(users).map(createUserMessage);
+// });
+// TODO 3.1 Update the user list display from step 2.3 so that it displays buttons, when clicked, draw events will only be dispatched to that user.
+const createUserBtn = username => new Button({
+  text: username,
+  variant: 'inverse',
+  mountPoint: userPanel,
+  onClick() {
+    toUser = username;
+    targetAudience.write(username);
+  },
+});
+socket.on('UPDATE_USER_LIST', ({users}) => {
+  userPanel.clear();
+  createAllUserBtn();
+  Object.keys(users).map(createUserBtn);
+});
+
+// TODO 3.3 create a button that, when clicked, will send draw events to all users again.
+const createAllUserBtn = () => new Button({
+  text: 'All',
+  mountPoint: userPanel,
+  onClick() {
+    toUser = undefined;
+    targetAudience.write('All');
+  },
+});
+createAllUserBtn();
